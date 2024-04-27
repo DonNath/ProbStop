a25a1ef0937bd056492429e635a19c26529bf7cd
Nathaniel Adebayo Ayodele <98522115+DonNath@users.noreply.github.com>
2024-04-27 13:47:48 +0100

    Add files via upload

---
+++ New folder/assets/js/script1.js
@@ -0,0 +1,21 @@
+
+// function submitForm(event) {
+//   event.preventDefault(); // Prevent form submission
+  
+//   // Fetch form values
+//   const fullName = document.getElementById('fullName').value;
+//   const email = document.getElementById('email').value;
+//   const bloodType = document.getElementById('bloodType').value;
+//   const organs = document.getElementById('organs').value;
+  
+//   // Simple form validation
+//   if (!fullName || !email) {
+//     alert('Please fill out all required fields!');
+//     return;
+//   }
+  
+//   // Simulate form submission (you would replace this with actual form submission logic)
+//   alert(`Thank you, ${fullName}! Your sign-up was successful.`);
+//   // Optionally, you can reset the form after submission
+//   document.getElementById('donorForm').reset();
+// }
